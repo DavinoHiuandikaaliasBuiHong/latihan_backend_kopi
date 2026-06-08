e293d9139a5637b24f91daac0994c4b6ec396812
Davino Hiuandika alias Bui Hong <davinohiuandika@itbss.ac.id>
2026-06-08 21:38:07 +0700

    8 June 2026

--- login.php
+++ login.php
@@ -1,10 +1,26 @@
 <?php
 include "header.php";
 ?>
-<div class="login">
+<style>
+    .login {
+    justify-content: center;
+    align-items: center;
+    display: flex;
+    flex-direction: column;
+    background-color: #f2f2f2;}
+
+    input {
+        margin: 5px;
+        width: 80%;
+    }
+</style>
+
+<section>
     <form action="sv_login.php" method="post">
+        <div class="login">
         <input type="text" placeholder="username" name="username"><br>
         <input type="password" placeholder="password" name="password"><br>
         <button type="submit">Login Bosqu!</button>
+        </div>
     </form>
-</div>
+</section>
